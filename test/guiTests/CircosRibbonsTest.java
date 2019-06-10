c10028ffe741cd4ce4d00100ab49c9008ff07d83
Paolo Pavan <paolo.pavan@gmail.com>
2019-06-10 16:28:29 +0100

    New GUI test added

---
+++ test/guiTests/CircosRibbonsTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guiTests;
+
+import circos.widget.Circos;
+import circos.widget.eventHandlers.ArcEventHandler;
+import circos.widget.eventHandlers.LinkEventHandler;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Scene;
+
+/**
+ *
+ * @author pavanpa
+ */
+public class CircosRibbonsTest extends BaseGuiTester{
+
+    @Override
+    void configureCircos(JFXPanel jfxPanel) {
+        Circos widget = new Circos(new long[]{34,56,90, 65, 10}, new ArcEventHandler(), new LinkEventHandler());
+        widget.setDrawRibbons(true);
+        widget.setStrokeWidth(1);
+        widget.setTitle("This is a test");
+        widget.initialize();
+        loadLinks(widget);
+        
+        jfxPanel.setScene(new Scene(widget));
+        startIncorporatedAnimation(widget);
+    }
+    
+    public static void main(String[] args){
+        BaseGuiTester me = new CircosRibbonsTest();
+        me.initSwing();
+    }
+    
+}
